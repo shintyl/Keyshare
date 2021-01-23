7a815034eee2aecd88bfe7f2a16c31aed925c5dc
Lawrence <DenouementX@users.noreply.github.com>
2021-01-23 03:11:00 -0500

    styling and font changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,12 +12,18 @@ const useStyles = makeStyles((theme) => ({
         left:'0px',
         position: 'fixed',
     },
+    entryId: {
+        fontSize: "0.8em",
+    },
+    fillText: {
+        padding: "10px",
+    }
 }));
 
 const theme = createMuiTheme({
   typography: {
     fontFamily: [
-      'Pineapple Grass',
+      'American Typewriter',
     ],
   },
 });
@@ -43,18 +49,18 @@ function App() {
         </header>
         <div className="Body">
           <img src={backgroundPhoto} alt="Background" className={classes.backgroundDiv} style={{zIndex:-1}}/>
-          <Typography variant='h4'>Enter your room ID:</Typography>
-	      <TextField name='roomId' variant='filled' size='small' required label='Room ID is required' onChange={updateValues}/>
+          <Typography variant='h4' className={classes.entryId}>Enter your room ID:</Typography>
+	      <TextField className={classes.fillText} name='roomId' variant='filled' size='small' required label='Room ID is required' onChange={updateValues}/>
           <Button variant="contained">
             Connect
 	      </Button>
         </div>
         <div className="Instructions">
-          <Paper elevation={3}>
-            <Typography variant='h6'>Instructions:</Typography>
+          <Paper elevation={10}>
+            <Typography className="topInstruction" variant='h6'>Instructions:</Typography>
             <Typography variant='h6'>1) Enter your instructor's room code above (which they should give you)</Typography>
             <Typography variant='h6'>2) Connect into the room by pressing the connect button</Typography>
-            <Typography variant='h6'>3) Plug in your keyboard to your computer, and start playing.</Typography>
+            <Typography className="bottomInstruction" variant='h6'>3) Plug in your keyboard to your computer, and start playing.</Typography>
           </Paper>
         </div>
       </div>
