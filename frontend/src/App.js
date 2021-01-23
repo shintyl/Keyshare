63f10bae8ccb89bb78b5044efa5a76144c7a5f77
Everett X <exu6@jh.edu>
2021-01-22 22:44:32 -0500

    Initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,32 @@
 import logo from './resources/logo.svg';
 import './App.css';
+import React from "react";
+import ReactDOM from 'react-dom';
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
+  const [values, setValues] = React.useState({
+    roomId: '',
+  });
+
+  const updateValues = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography variant='h4'>Room ID</Typography>
+	<TextField name='roomId' onChange={updateValues} required label="required"/>
+        <Button variant="contained" href = "./pages/temp.js">
+          <Typography variant='h5'>Connect</Typography>
+	    </Button>
       </header>
     </div>
   );
