c0749d39b2db7824c3cfe6ca899ea7e326c00795
shintyl <tylerjshin@gmail.com>
2021-01-23 17:58:39 +0000

    firebase

--- frontend/src/services/connectToPeer.js
+++ frontend/src/services/connectToPeer.js
@@ -1,1 +1,12 @@
 import firebase from 'firebase.js'
+
+async function createRoom() {
+    const db = firebase.firestore()
+    const roomRef = await db.collection('rooms').doc();
+    console.log('Create PeerConnection with configuration: ', configuration);
+    peerConnection = new RTCPeerConnection(configuration);
+}
+
+function registerPeerConnectionListeners() {
+    
+}
