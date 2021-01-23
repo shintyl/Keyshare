dbecb8d840523b1aa361b9d393d6cc6a7c2920d5
Everett X <exu6@jh.edu>
2021-01-23 02:17:03 -0500

    Font work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import backgroundPhoto from './resources/keyshare-bg.png';
 import './App.css';
 import React from "react";
-import makeStyles from "@material-ui/core/styles/makeStyles";
+import {createMuiTheme, makeStyles, ThemeProvider} from "@material-ui/core/styles";
 import {Button, Paper, TextField, Typography} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
@@ -13,6 +13,14 @@ const useStyles = makeStyles((theme) => ({
         position: 'fixed',
     },
 }));
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Pineapple Grass',
+    ],
+  },
+});
 
 function App() {
   const [values, setValues] = React.useState({
@@ -29,26 +37,28 @@ function App() {
   const classes = useStyles();
 
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <div className="Body">
-        <img src={backgroundPhoto} alt="Background" className={classes.backgroundDiv} style={{zIndex:-1}}/>
-        <Typography variant='h4'>Enter your room ID:</Typography>
-	    <TextField name='roomId' variant='filled' size='small' required label='Room ID is required' onChange={updateValues}/>
-        <Button variant="contained">
-          Connect
-	    </Button>
+    <ThemeProvider theme = {theme}>
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <div className="Body">
+          <img src={backgroundPhoto} alt="Background" className={classes.backgroundDiv} style={{zIndex:-1}}/>
+          <Typography variant='h4'>Enter your room ID:</Typography>
+	      <TextField name='roomId' variant='filled' size='small' required label='Room ID is required' onChange={updateValues}/>
+          <Button variant="contained">
+            Connect
+	      </Button>
+        </div>
+        <div className="Instructions">
+          <Paper elevation={3}>
+            <Typography variant='h6'>Instructions:</Typography>
+            <Typography variant='h6'>1) Enter your instructor's room code above (which they should give you)</Typography>
+            <Typography variant='h6'>2) Connect into the room by pressing the connect button</Typography>
+            <Typography variant='h6'>3) Plug in your keyboard to your computer, and start playing.</Typography>
+          </Paper>
+        </div>
       </div>
-      <div className="Instructions">
-        <Paper elevation={3}>
-          <Typography variant='h6'>Instructions:</Typography>
-          <Typography variant='h6'>1) Enter your instructor's room code above (which they should give you)</Typography>
-          <Typography variant='h6'>2) Connect into the room by pressing the connect button</Typography>
-          <Typography variant='h6'>3) Plug in your keyboard to your computer, and start playing.</Typography>
-        </Paper>
-      </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
