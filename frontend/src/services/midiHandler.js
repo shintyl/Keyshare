fb7cf681d6188f682b544a4f525024b02c5014b6
Everett X <exu6@jh.edu>
2021-01-23 01:06:15 -0500

    Initial commit

---
+++ frontend/src/services/midiHandler.js
@@ -0,0 +1,15 @@
+//Return an array with inputs and outputs
+function getInputsAndOutputs() {
+	navigator.requestMIDIAccess().then(
+		function success(midiAccess) {
+			return [midiAccess.inputs,midiAccess.outputs];
+		}, function failure() {
+			window.alert('Could not automatically get a MIDI device. Make sure that you are using a compatible browser.')
+		}
+	);
+}
+
+//Play note to MIDI output with note info array (formatted as [on/off, note, velocity])
+function playNote = (noteInfo, midiOutput) => {
+	midiOutput.send([noteInfo[0],noteInfo[1],noteInfo[2]]);
+}
