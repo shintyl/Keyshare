3e716543f6678e31e7b99fdf51f2f71b03393029
Everett X <exu6@jh.edu>
2021-01-23 03:34:33 -0500

    Added toggle button functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -54,7 +54,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <div className="Selector">
-            <ToggleButtonGroup exclusive value={selection} onChange={updateSelection} aria-label="I am a">
+            <ToggleButtonGroup exclusive value={selection} onChange={updateSelection} aria-label="userStatus">
               <ToggleButton value="student" aria-label="left aligned">
                 Student
               </ToggleButton>
@@ -66,18 +66,58 @@ function App() {
         </header>
         <div className="Body">
           <img src={backgroundPhoto} alt="Background" className={classes.backgroundDiv} style={{zIndex:-1}}/>
-          <Typography variant='h4' className={classes.entryId}>Enter your room ID:</Typography>
-	      <TextField className={classes.fillText} name='roomId' variant='filled' size='small' required label='Room ID is required' onChange={updateValues}/>
+          {selection === "student" ?
+            <div>
+              <Typography variant='h4' className={classes.entryId}>
+                Enter your room ID:
+              </Typography>
+              <TextField className={classes.fillText} name='roomId' variant='filled' size='small' required label='Room ID is required' onChange={updateValues}/>
+            </div>
+            :
+            <div>
+              <Typography variant='h4' className={classes.entryId}>
+                Use room ID:
+              </Typography>
+              <TextField className={classes.fillText} name='genRoomId' variant='filled' size='small' label='Your room ID (read-only)' defaultValue="Hello World" InputProps={{readOnly: true,}}/>
+            </div>
+          }
           <Button variant="contained">
             Connect
 	      </Button>
         </div>
         <div className="Instructions">
           <Paper elevation={10}>
-            <Typography className="topInstruction" variant='h6'>Instructions:</Typography>
-            <Typography variant='h6'>1) Enter your instructor's room code above (which they should give you)</Typography>
-            <Typography variant='h6'>2) Connect into the room by pressing the connect button</Typography>
-            <Typography className="bottomInstruction" variant='h6'>3) Plug in your keyboard to your computer, and start playing.</Typography>
+            {selection === "student" ?
+              <div>
+                <Typography className="topInstruction" variant='h6'>
+                  Instructions:
+                </Typography>
+                <Typography variant='h6'>
+                  1) Enter your instructor's room code above (which they should give you)
+                </Typography>
+                <Typography variant='h6'>
+                  2) Connect into the room by pressing the connect button
+                </Typography>
+                <Typography className="bottomInstruction" variant='h6'>
+                  3) Plug your MIDI instrument into your computer, and start playing.
+                </Typography>
+              </div>
+              :
+              <div>
+                <Typography className="topInstruction" variant='h6'>
+                  Instructions:
+                </Typography>
+                <Typography variant='h6'>
+                  1) Give your student the auto-generated room code above.
+                </Typography>
+                <Typography variant='h6'>
+                  2) Wait until they connect, at which point you will be automatically redirected.
+                </Typography>
+                <Typography className="bottomInstruction" variant='h6'>
+                  3) Wait for your student to plug their MIDI instrument in, and start listening.
+                </Typography>
+              </div>
+            }
           </Paper>
         </div>
       </div>
